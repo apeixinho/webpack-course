7b7b1071034296d1c639deca4fad960960cc8e9d
A. Peixinho <a_peixinho@sapo.pt>
2018-07-29 01:22:36 +0200

    deleted dist

--- dist/Article.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{20:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u,l=(u=n(1))&&u.__esModule?u:{default:u};n(71),t.default=function(){return l.default.createElement("div",null,l.default.createElement("h1",null,"Article"))}},71:function(e,t){}}]);
